075f9245546f3053fbeda53a616ce70d721c4d92
egosteva <elenagosteva.qa@gmail.com>
2023-05-06 01:24:59 +0300

    formatting

--- src/test/java/com/github/egosteva/config/WebDriverConfig.java
+++ src/test/java/com/github/egosteva/config/WebDriverConfig.java
@@ -7,12 +7,12 @@ import org.aeonbits.owner.Config;
 })
 
 public interface WebDriverConfig extends Config {
-
     @Key("browser")
     @DefaultValue("chrome")
     String getBrowser();
 
     @Key("browserVersion")
+    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("browserSize")
